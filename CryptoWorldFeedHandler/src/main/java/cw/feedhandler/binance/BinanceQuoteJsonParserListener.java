1294411b6b261f0b31d3fac6ff6ea6abc3cdbe59
yongan2 <yongan2@gmail.com>
2021-12-26 23:15:00 -0500

    quote json parser for binance and test

---
+++ CryptoWorldFeedHandler/src/main/java/cw/feedhandler/binance/BinanceQuoteJsonParserListener.java
@@ -0,0 +1,86 @@
+package cw.feedhandler.binance;
+
+import cw.common.json.JsonNumber;
+import cw.common.json.JsonParserListenerAdaptor;
+
+import java.util.Arrays;
+
+public class BinanceQuoteJsonParserListener extends JsonParserListenerAdaptor {
+    private static final String LAST_UPDATE_ID = "lastUpdateId";
+    private static final String STREAM = "stream";
+    private static final String BIDS = "bids";
+    private static final String ASKS = "asks";
+
+    private boolean foundLastUpdateId;
+    private boolean[] foundBids;
+    private boolean[] foundAsks;
+
+    long lastUpdateId;
+    StringBuilder stream;
+    double bidPrice;
+    double bidSize;
+    double askPrice;
+    double askSize;
+
+    public BinanceQuoteJsonParserListener() {
+        this.foundLastUpdateId = false;
+        this.foundBids = new boolean[2];
+        this.foundAsks = new boolean[2];
+
+        this.stream = new StringBuilder();
+    }
+
+    @Override
+    public boolean onObjectMember(CharSequence name) {
+        int length = name.length();
+
+        if ((STREAM.length() == length) && STREAM.equals(name.toString())) {
+            this.stream.setLength(0);
+        } else if ((LAST_UPDATE_ID.length() == length) && LAST_UPDATE_ID.equals(name.toString())) {
+            this.foundLastUpdateId = true;
+        } else if ((BIDS.length() == length) && BIDS.equals(name.toString())) {
+            Arrays.fill(this.foundBids, true);
+        } else if ((ASKS.length() == length) && ASKS.equals(name.toString())) {
+            Arrays.fill(this.foundAsks, true);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onNumberValue(JsonNumber number) {
+        if (this.foundLastUpdateId) {
+            this.lastUpdateId = number.mantissa();
+            this.foundLastUpdateId = false;
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onStringValue(CharSequence data) {
+        if (this.stream.length() == 0) {
+            this.stream.append(data);
+            return false;
+        } else if (this.foundBids[0]) {
+            this.bidPrice = Double.parseDouble(data.toString());
+            this.foundBids[0] = false;
+            return false;
+        } else if (this.foundBids[1]) {
+            this.bidSize = Double.parseDouble(data.toString());
+            this.foundBids[1] = false;
+            return false;
+        } else if (this.foundAsks[0]) {
+            this.askPrice = Double.parseDouble(data.toString());
+            this.foundAsks[0] = false;
+            return false;
+        } else if (this.foundAsks[1]) {
+            this.askSize = Double.parseDouble(data.toString());
+            this.foundAsks[1] = false;
+            return false;
+        }
+
+        return true;
+    }
+}
