3a83134f00baa7c80242d9b340813ebed3ab283a
yongan2 <yongan2@gmail.com>
2021-12-26 23:16:58 -0500

    enhance TradingPair with static map to handle responses

--- CryptoWorldCommon/src/main/java/cw/common/md/TradingPair.java
+++ CryptoWorldCommon/src/main/java/cw/common/md/TradingPair.java
@@ -1,5 +1,6 @@
 package cw.common.md;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public enum TradingPair {
@@ -9,6 +10,19 @@ public enum TradingPair {
     ETHUSDT(Map.of(
             Exchange.BINANCE, "ethusdt",
             Exchange.KUCOIN, "ETH-USDT"));
+
+    public static final Map<Exchange, Map<String, TradingPair>> EXCHANGE_TO_SYMBOL_TO_TRADING_PAIR;
+
+    static {
+        EXCHANGE_TO_SYMBOL_TO_TRADING_PAIR = new HashMap<>();
+
+        for (TradingPair tradingPair : TradingPair.values()) {
+            for (Map.Entry<Exchange, String> entry : tradingPair.getExchangeToSymbolMap().entrySet()) {
+                Map<String, TradingPair> symbolToTradingPair = EXCHANGE_TO_SYMBOL_TO_TRADING_PAIR.computeIfAbsent(entry.getKey(), e -> new HashMap<>());
+                symbolToTradingPair.put(entry.getValue(), tradingPair);
+            }
+        }
+    }
 
     private final Map<Exchange, String> exchangeToSymbolMap;
 
